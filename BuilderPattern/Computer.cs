461db472391c8550827d32b3f747a5d1f2ae59d3
Gavin_Gao <2508379962@qq.com>
2024-04-27 17:08:06 +0800

    add the builder pattern code

---
+++ BuilderPattern/Computer.cs
@@ -0,0 +1,26 @@
+﻿namespace BuilderPattern
+{
+    public class Computer
+    {
+        public ComputerBuilder computerBuilder;
+
+        public Computer(ComputerBuilder builder)
+        {
+            this.computerBuilder = builder;
+        }
+        public ComputerBuilder Construct()
+        {
+            computerBuilder.CreateCPU();
+            computerBuilder.CreateHardDisk();
+            computerBuilder.CreateMemory();
+            if (computerBuilder.IsDisplay())
+            {
+                computerBuilder.CreateDisplay();
+            }
+            var Type = computerBuilder.GetType().Name;
+           
+            Console.WriteLine("{0}生产成功", Type);
+            return computerBuilder;
+        }
+    }
+}
